befa5e99d09844a52caa88186ce6190af6959f99
AasthaRai17 <77830618+AasthaRai17@users.noreply.github.com>
2021-02-19 14:47:21 +0530

    Delete src/com/planon directory

--- src/com/planon/employee/StaffMembers.java
+++
@@ -1,9 +0,0 @@
-package com.planon.employee;
-
-import com.planon.ticket.Ticket;
-
-public interface StaffMembers extends Employee{
-	public void doTheAssignedTask(Ticket ticket);
-	public String getStaffId();
-	public String getStaffMemberName();
-}
